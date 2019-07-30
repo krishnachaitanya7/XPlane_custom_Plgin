4af1e9be6d38b74f1118685e8562980257fa6269
krishna <chaitanyaradon89@gmail.com>
2019-07-30 00:53:17 -0600

    Made some changes, going to sleep now.

--- Qt_Plugin/mainwindow.cpp
+++ Qt_Plugin/mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+#include <QtWidgets/QGridLayout>
 
 MainWindow::MainWindow(QWidget *parent) :
         QMainWindow(parent),
         ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QGridLayout *grid = new QGridLayout;
+    grid->addWidget(createFirstQuestion(), 0, 0);
+    grid->addWidget(createSecondQuestion(), 1, 0);
+    setLayout(grid);
+
+    setWindowTitle(tr("Group Boxes"));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+QGroupBox *MainWindow::createFirstQuestion() {
+    QGroupBox *groupBox = new QGroupBox(tr("Did you enjoy XPlane experience?"));
+
+    QRadioButton *radio1 = new QRadioButton(tr("Yes"));
+    QRadioButton *radio2 = new QRadioButton(tr("No"));
+    QRadioButton *radio3 = new QRadioButton(tr("Maybe"));
+    QRadioButton *radio4 = new QRadioButton(tr("God Knows"));
+
+    radio1->setChecked(true);
 }
--- Qt_Plugin/mainwindow.h
+++ Qt_Plugin/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtWidgets/QGroupBox>
 
 namespace Ui {
     class MainWindow;
@@ -15,8 +16,13 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_buttonBox_accepted();
+    void on_radioButton_clicked();
 private:
     Ui::MainWindow *ui;
+    QGroupBox *createFirstQuestion();
+    QGroupBox *createSecondQuestion();
 };
 
 #endif // MAINWINDOW_H
